23e82deb9c4dfdc974c88252cb0d9933292490be
EngieDev <admin@engiedev.net>
2021-12-30 21:32:28 +0000

    Added configuration loader & sample config

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type pingTarget struct {
+	Name    string `json:"name"`
+	Host    string `json:"host"`
+	Timeout int    `json:"timeout"`
+	Colour  string `json:"colour"`
+	Period  int    `json:"period"`
+}
+
+type config struct {
+	ServerIP    string       `json:"serverIP"`
+	ServerPort  int          `json:"serverPort"`
+	PingTargets []pingTarget `json:"pingTargets"`
+}
+
+func main() {
+	// Load configuration file - conf.json
+	var Config config
+	loadConfig(&Config)
+	fmt.Println("Loaded config")
+
+	fmt.Println(Config.PingTargets)
+}
+
+// Function to load configuration file into config struct
+// 	- Doesn't add default values
+// 	- conf.json only for now
+func loadConfig(Config *config) {
+	conf, err := os.Open("conf.json")
+	if err != nil {
+		fmt.Println("Failed to open configuration file")
+		checkError(err)
+	}
+	defer conf.Close()
+
+	configBytes, err := ioutil.ReadAll(conf)
+	checkError(err)
+
+	json.Unmarshal(configBytes, &Config)
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+}
