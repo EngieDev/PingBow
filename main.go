860e48353df2ecd85c6462f8739436c014a8ca9c
EngieDev <admin@engiedev.net>
2021-12-30 21:36:02 +0000

    Comment to explain checkError

--- main.go
+++ main.go
@@ -47,6 +47,8 @@ func loadConfig(Config *config) {
 	json.Unmarshal(configBytes, &Config)
 }
 
+// Quick helper function to save my fingers
+// When err != nil, dumps error and panics
 func checkError(err error) {
 	if err != nil {
 		fmt.Println(err)
