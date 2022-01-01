f2f7e4d9400a666b75fd4d5e39813dc41838dbaa
EngieDev <admin@engiedev.net>
2022-01-01 18:57:00 +0000

    Pinger function added

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"time"
+
+	"github.com/go-ping/ping"
 )
 
 type pingTarget struct {
@@ -27,7 +31,65 @@ func main() {
 	loadConfig(&Config)
 	fmt.Println("Loaded config")
 
-	fmt.Println(Config.PingTargets)
+	// Checks if any targets are configured
+	if len(Config.PingTargets) == 0 {
+		checkError(errors.New("no ping targets configured"))
+	}
+
+	// Iterates over the PingTargets and spawns a pinger instance
+	fmt.Println("Creating pinger instances")
+	for i := range Config.PingTargets {
+		go pinger(&Config.PingTargets[i])
+	}
+
+	time.Sleep(20 * time.Second)
+}
+
+// Takes a pingTarget, checks the validity then pings the target
+// Currently just dumps the data to stdout
+// Meant to be used as a goroutine
+func pinger(target *pingTarget) {
+	// Checks if host is set, otherwise quits
+	if target.Host == "" {
+		checkError(errors.New("one of the ping targets is missing a hostname/ip"))
+	}
+
+	// Checks if name is set, defaults to hostname if its not
+	if target.Name == "" {
+		fmt.Println("Missing name for one of ping targets, defaulting to hostname")
+		target.Name = target.Host
+	}
+
+	// Default timeout setting
+	if target.Timeout == 0 {
+		target.Timeout = 3
+	}
+
+	// Default colour setting
+	if target.Colour == "" {
+		target.Colour = "purple"
+	}
+
+	// Default period setting
+	if target.Period == 0 {
+		target.Period = 5
+	}
+
+	timeout := time.Duration(target.Timeout) * time.Second
+
+	// Pings forever
+	for {
+		// Have to recreate pinger?
+		Pinger, err := ping.NewPinger(target.Host)
+		checkError(err)
+
+		Pinger.Timeout = timeout
+		err = Pinger.Run()
+		checkError(err)
+
+		fmt.Println(target.Host, Pinger.Statistics())
+		time.Sleep(time.Second * time.Duration(target.Period))
+	}
 }
 
 // Function to load configuration file into config struct
